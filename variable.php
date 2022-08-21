0b27402781c3389d2253a9ced6468a85ff254643
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-21 14:21:54 +0600

    variable task completed

---
+++ variable.php
@@ -0,0 +1,11 @@
+<?php
+/*
+* author: Md. Shofiul Islam
+* Brainstation-23
+* Topic: variable
+*/
+$name = "Md. Shofiul Islam";
+$age  = 24;
+
+echo "Hello, I'm {$name},and I'm {$age} years old";
+?>
