e97a9b518de4f4661b9f973fe7dc7ca35c0568d4
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-21 18:28:32 +0600

    classAndObject.php implemented

--- classAndObject.php
+++ classAndObject.php
@@ -5,6 +5,25 @@
 * Topic:  Class and Object
 */
 
+class Person{
+    private $name;
+    private $age;
+    
+    function set_name($name){
+        $this->name = $name;
+    } 
+    function set_age($age){
+        $this->age = $age;
+    }
+    function print_info(){
+        echo "Name is: {$this->name} </br>";
+        echo "Age is: {$this->age} </br>";
+    }
+}
 
+$person = new Person();
+$person->set_name("Alava");
+$person->set_age(30);
+$person->print_info();
 
 ?>
