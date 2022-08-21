a459c0a257acf9dae57700345dd53a9e1c597bfa
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-21 15:09:38 +0600

    arrays.php implemented

--- arrays.php
+++ arrays.php
@@ -5,4 +5,14 @@
 * Topic: Arrays
 */
 
+$arr = [1,2,3,4,5]; //Regular array
+$associated_array = ["name"=>"Shofiul","age"=>24];
+
+echo "Regular array:";
+var_dump($arr);
+
+
+echo "</br> Associated array:";
+var_dump($associated_array);
+
 ?>
