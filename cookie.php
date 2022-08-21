2c6ad529018272a9d6af0a6e089310419efb1223
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-21 17:20:17 +0600

    cookie.php implemented

--- cookie.php
+++ cookie.php
@@ -4,6 +4,9 @@
 *         Brainstation-23
 * Topic:  Cookie
 */
+setcookie('username',"James Albert",time()+30); //this cookie will distroy after 30 seconds.
 
+$username = $_COOKIE['username']; // retrieving the user name from cookie. 
+echo "<h1> Welcome to the page, <i>{$username}</i></h1>";
 
 ?>
