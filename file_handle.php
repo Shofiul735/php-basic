1708a74a22a048a23fc2536f723bf94cf344f903
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-21 15:39:14 +0600

    file_handle.php implemented

--- file_handle.php
+++ file_handle.php
@@ -5,5 +5,16 @@
 * Topic: variable
 */
 
+// File read
+$fileRead = fopen("input.txt","r") or die("Unable to open");
+$text = fread($fileRead,filesize('input.txt'));
+echo $text;
+
+// File write
+$fileWrite = fopen("output.txt","a") or die("Unable to open");
+fwrite($fileWrite,$text);
+fclose($fileRead);
+fclose($fileWrite);
+
 
 ?>
