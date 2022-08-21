29747089bce7a17d97b61578bba207edddffb79e
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-21 14:50:34 +0600

    loop.php completed

--- loops.php
+++ loops.php
@@ -4,5 +4,30 @@
 * Brainstation-23
 * Topic: Loops
 */
+$arr = [1,2,3,4,5];
+echo "For loop:";
+for($i = 0;$i<sizeof($arr);$i++){
+    echo "{$arr[$i]} ";
+}
+
+echo "</br>While loop:";
+$i = 0;
+while($i<sizeof($arr)){
+    echo "{$arr[$i]} ";
+    $i++;
+}
+
+echo "</br> do while loop:";
+$i = 0;
+do{
+    echo "{$arr[$i]} ";
+    $i++;  
+}while($i<sizeof($arr));
+
+
+echo "</br> For each loop:";
+foreach($arr as $index=>$item){
+    echo "</br>value {$item},and index {$index}";
+}
 
 ?>
