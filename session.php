93842c09b64dba335484f2cfaa7ff6b47f5f65f2
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-21 17:11:58 +0600

    session.php implemented

--- session.php
+++ session.php
@@ -2,7 +2,13 @@
 /*
 * author: Md. Shofiul Islam
 *         Brainstation-23
-* Topic: variable
+* Topic: Session
 */
-
+session_start();
+if(isset($_SESSION['visit'])){
+    $_SESSION['visit']++;
+}else{
+    $_SESSION['visit'] = 1;
+}
+echo "<h1>You have refreshed this page {$_SESSION['visit']} times.</h1>";
 ?>
