360af950790800b9303b1861193747fca029b603
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-21 14:30:57 +0600

    dataTypes.php completed

---
+++ dataTypes.php
@@ -0,0 +1,34 @@
+<?php
+/*
+* author: Md. Shofiul Islam
+* Brainstation-23
+* Topic: variable types
+*/
+
+// Integer type
+$num0 = 10;
+var_dump($num0);
+echo "</br>";
+
+//Float type
+$num1 = 9.99;
+var_dump($num1);
+echo "</br>";
+
+// Boolean Type
+$isIt = true;
+var_dump($isIt);
+echo "</br>";
+
+// String type
+$name = "Md. Shofiul Islam";
+var_dump($name);
+echo "</br>";
+
+// Array 
+$intArray = [10, 20 , 30];
+var_dump($intArray);
+echo "</br>";
+
+
+?>
