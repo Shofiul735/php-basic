f857c988cde5b5998da2bb07b0130cbeeb33bba7
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-21 15:21:49 +0600

    dateAndTime.php implemented

--- dateAndTime.php
+++ dateAndTime.php
@@ -1,8 +1,25 @@
 <?php
 /*
 * author: Md. Shofiul Islam
-* Brainstation-23
+*         Brainstation-23
 * Topic: Date and Time
 */
+
+
+// Date
+$todaysDate_year_first = date("Y/m/d");
+echo "Todays date is: {$todaysDate_year_first} </br>";
+
+$todaysDate_month_first = date("m/d/Y");
+echo "Todays date is: {$todaysDate_month_first} </br>";
+
+$todaysDate_day_first = date("d/m/Y");
+echo "Todays date is: {$todaysDate_day_first} </br>";
+
+
+//time
+date_default_timezone_set("Asia/Dhaka"); //setting timezone to dhaka.
+$time =  date("h:i:sa"); // h- hour in 12hr formate,i- minutes s-second a-AM/PM
+echo "The time is:{$time}";
 
 ?>
