ab0a45196a8c09f5335beb8194812058cc83a599
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-21 14:40:34 +0600

    Operators.php completed

---
+++ operators.php
@@ -0,0 +1,34 @@
+<?php
+/*
+* author: Md. Shofiul Islam
+* Brainstation-23
+* Topic: Operators
+*/
+$a = 5;
+$b = 3;
+echo "Value of a is: {$a}.</br> Value of b is: {$b}</br>";
+
+// Addition
+$sum = $a + $b;
+echo "Sum of a and b is:{$sum} </br>";
+
+//Subtraction
+$sub = $a - $b;
+echo "Subtraction of a from b is:{$sub}</br>";
+
+//Multiplication
+$multi = $a * $b;
+echo "Multiplication of a and b is:{$multi}</br>";
+
+//Division
+$div = $a / $b;
+echo "Division of a and b is:{$div}</br>";
+
+//Modulus
+$mod = $a % $b;
+echo "Modulus of a and b is:{$mod}</br>";
+
+//Exponentiation
+$expo = $a ** $b;
+echo "Exponentiation of a and b is:{$expo}</br>";
+?>
