c09d6aadbb4ec7b09f858f316744e8a1d84b624a
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-22 13:23:43 +0600

    constructorAndDestructor.php implemented

--- constructorAndDestructor.php
+++ constructorAndDestructor.php
@@ -5,5 +5,15 @@
 * Topic:  Constructor and Destructor
 */
 
+class Car{
+    public function __construct(){
+        echo "I'm constructor.</br>";
+    }
+    public function __destruct(){
+        echo "I'm destructor.</br>";
+    }
+}
+
+$car = new Car();
 
 ?>
