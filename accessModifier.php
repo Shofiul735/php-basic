193646691a707a3428dd344b3f859e1364c0ec9c
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-22 13:34:43 +0600

    accessModifier.php implemented

--- accessModifier.php
+++ accessModifier.php
@@ -5,4 +5,28 @@
 * Topic:  Access Modifier
 */
 
+class Person{
+    /*
+        Access modifiers are:
+            1. Public     -> access from everywhere
+            2. Private    -> only in the class
+            3. Protected  -> within class and child class
+    */
+    private $name;    // only class methods can access this
+    private $message; // only class methods can access this
+
+    public function __construct($name){
+        $this->name = $name;
+    }
+    protected function message(){
+        $this->message = "Hello, {$this->name}";
+    }
+    public function __destruct(){
+        $this->message();
+        echo $this->message;
+    }
+}
+
+$person = new Person("Alen");
+
 ?>
