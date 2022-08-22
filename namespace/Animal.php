dae76163eead27f53a76095f2de22d041bc32517
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-22 15:08:03 +0600

    namespace implemented

---
+++ namespace/Animal.php
@@ -0,0 +1,10 @@
+<?php
+include_once 'namespace.php';
+
+use Animal\Cat;
+
+
+$cat = new Cat();
+$cat->makeSound();
+
+?>
---
+++ namespace/namespace.php
@@ -0,0 +1,16 @@
+<?php
+/*
+* author: Md. Shofiul Islam
+*         Brainstation-23
+* Topic:  php namespace
+*/
+
+namespace Animal;
+
+class Cat{
+    public function makeSound(){
+        echo "<h1>Mewao</h1>";
+    }
+}
+
+?>
