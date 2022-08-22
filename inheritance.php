c434aad07d8585606763a8384fe39c3a79f3a10c
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-22 13:41:56 +0600

    inheritance.php implemented

--- inheritance.php
+++ inheritance.php
@@ -5,4 +5,24 @@
 * Topic:  Inheritance
 */
 
+class Fruit {
+    public $name;
+    public $color;
+    public function __construct($name, $color) {
+      $this->name = $name;
+      $this->color = $color;
+    }
+    protected function intro() {
+      echo "The fruit is {$this->name} and the color is {$this->color}.";
+    }
+  }
+  
+  class Mango extends Fruit {
+    public function message() {
+      echo "Am I a fruit or a Mango? ";
+    }
+  }
+
+  $mango = new Mango("Mango","Yellow");
+  $mango->message();
 ?>
