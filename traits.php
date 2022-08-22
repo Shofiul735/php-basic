697eef8a76caacdd5e551699c6b831538a3c84cf
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-22 14:51:49 +0600

    traits.php implemented

--- traits.php
+++ traits.php
@@ -2,9 +2,26 @@
 /*
 * author: Md. Shofiul Islam
 *         Brainstation-23
-* Topic:  Traits
+* Topic:  Traits for multi inheritace
 */
 
+trait ProgrammingLanguage1{
+    public function JS(){
+        echo "JavaScript is the best programming language ever created. </br>";
+    }
+}
+trait ProgrammingLanguage2{
+    public function CplusPlus(){
+        echo "C++ is the most powerful oop language. </br>";
+    }
+}
 
+class ProgrammingLanguage{
+    use ProgrammingLanguage1;
+    use ProgrammingLanguage2;
+}
 
+$pl = new ProgrammingLanguage();
+$pl->JS();
+$pl->CplusPlus();
 ?>
