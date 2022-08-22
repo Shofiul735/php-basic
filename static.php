61a3ccd15b0deb84477704d93269fdbcfb7fad75
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-22 14:57:05 +0600

    static.php implemented

--- static.php
+++ static.php
@@ -5,4 +5,18 @@
 * Topic:  Static method and properties
 */
 
+
+class Person{
+    static $count = 10;
+    public static greetings(){
+        echo "<h1>Hello</h1> </br>";
+    }
+
+}
+
+
+// static methods and properties can be access without creating an object.
+echo Person::$count
+echo Person::greetings();
+
 ?>
