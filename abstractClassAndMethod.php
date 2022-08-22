114a425247aac2ef354bf777446ff32913cb944c
Md. Shofiul Islam <shofiul.islam@brainstation-23.com>
2022-08-22 14:38:53 +0600

    abstractClassAndMethod.php implemented

--- abstractClassAndMethod.php
+++ abstractClassAndMethod.php
@@ -2,8 +2,24 @@
 /*
 * author: Md. Shofiul Islam
 *         Brainstation-23
-* Topic:  Cookie
+* Topic:  Abstract class and method
 */
+abstract class Animal{
+    private $type;
+    public function __construct($type){
+        $this->type = $type;
+    }
+    abstract public function info();
+}
 
+class Human extends Animal{
+    public function info(){
+        echo "<h1>I am human</h1> </br>";
+    }
+}
+
+
+$human = new Human("Human");
+$human->info();
 
 ?>
